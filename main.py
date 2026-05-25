b7d9e41e05c35c0a0776d023092168abea897d3c
legendisa <124398449+legendisa@users.noreply.github.com>
2026-05-25 11:27:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.button import Button
+from jnius import autoclass
+
+# Android TTS sınıflarını çağır
+PythonActivity = autoclass('org.kivy.android.PythonActivity')
+TextToSpeech = autoclass('android.speech.tts.TextToSpeech')
+Locale = autoclass('java.util.Locale')
 
 class InaApp(App):
     def build(self):
-        return Label(text="İna sistemi hazır. APK başarıyla derlendi.")
+        # Ses motorunu başlat
+        self.tts = TextToSpeech(PythonActivity.mActivity, None)
+        self.tts.setLanguage(Locale.TR) # Türkçe dil ayarı
+        
+        btn = Button(text="Konuştur")
+        btn.bind(on_release=self.konus)
+        return btn
+
+    def konus(self, instance):
+        metin = "Sisteme başarıyla entegre edildim. Artık seninle konuşabiliyorum."
+        self.tts.speak(metin, TextToSpeech.QUEUE_FLUSH, None)
 
 if __name__ == '__main__':
     InaApp().run()
